73ca57651e8cf682228dff61fd98b3b7fa8afe28
emma_dwamena <emma.dwamena@gmail.com>
2025-08-14 22:48:47 +0000

    Update sarcasm_app.py

--- sarcasm_app.py
+++ sarcasm_app.py
@@ -662,8 +662,28 @@ _tab_labels = [
 _tabs = st.tabs(_tab_labels)
 
 with _tabs[0]:
-    st.markdown("### About")
-    st.write("This interface provides data upload/overview, preprocessing, model training & evaluation, and a prediction interface.")
+    st.markdown("""
+    <div style="background-color: #f2f7f7; padding: 2rem; border-radius: 1rem; margin-bottom: 2rem;">
+        <h2 style="color: #030a0a; text-align: center;">📌 About This App</h2>
+        <p style="text-align:center;max-width:900px;margin:0 auto;">
+            This dashboard predicts customer churn and provides EDA, preprocessing,
+            model training and evaluation.
+        </p>
+    </div>
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+    <div style="background-color: #f2f7f7; padding: 2rem; border-radius: 1rem; margin-bottom: 2rem;">
+        <h2 style="color: #030a0a; text-align: center;">👥Team Members</h2>
+        <div style="display: flex; justify-content: space-around; flex-wrap: wrap;">
+            <div>• Ruth Mensah - 22253087</div>
+            <div>• Emmanuel Oduro Dwamena - 11410636</div>
+            <div>• Zoe Akua Ohene-Ampofo - 22252412</div>
+            <div>• Sandra Animwaa Bamfo - 22256394</div>
+            <div>• Joshua Kwaku Mensah - 22257672</div>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
 
 with _tabs[1]:
     page_upload()
